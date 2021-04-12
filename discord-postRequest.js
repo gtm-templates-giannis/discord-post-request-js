3694d58d98e1cb06fee40554993c1b390ae853ae
Giannis Theoropoulos <jtheogr@gmail.com>
2021-04-12 21:30:12 +0300

    Fix
    Fix

--- discord-postRequest.js
+++ discord-postRequest.js
@@ -3,7 +3,7 @@ if (document.location.href.search('appspot.com') == -1) {
 var xhr = new XMLHttpRequest();
   var stringifiedData = JSON.stringify(data);
   xhr.open('POST', endpoint);
-  xhr.setRequestHeader('Content-type', 'application/json');
+  xhr.setRequestHeader('Content-type', 'application/json');//
   xhr.send(stringifiedData);
   xhr.onload = function () {
     if (xhr.status.toString()[0] !== '2') {
