51c19ffe793ff266eb288a1088b8d34b559e7277
Giannis Theoropoulos <jtheogr@gmail.com>
2021-04-12 21:29:25 +0300

    Update discord-postRequest.js

--- discord-postRequest.js
+++ discord-postRequest.js
@@ -2,8 +2,8 @@ function sendData(data, endpoint) {
 if (document.location.href.search('appspot.com') == -1) {
 var xhr = new XMLHttpRequest();
   var stringifiedData = JSON.stringify(data);
-  xhr.setRequestHeader("Content-Type", "application/json");
   xhr.open('POST', endpoint);
+  xhr.setRequestHeader('Content-type', 'application/json');
   xhr.send(stringifiedData);
   xhr.onload = function () {
     if (xhr.status.toString()[0] !== '2') {
